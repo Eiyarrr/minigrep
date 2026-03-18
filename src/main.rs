8a168ee10178b062f2d54625edbd031a260890f3
Eiyarrr <eiyar@eiyar.com>
2026-03-18 08:12:55 +0000

    Added line numbers, printing of lines, match counts

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,40 @@
+use std::fs::File;
 use std::io;
+use std::io::Error;
+use std::io::{BufRead, BufReader};
 
-fn main() {
+fn main() -> Result<(), Error> {
     let path = get_path();
     let query = get_query();
+    println!("Searching...");
 
-    println!("path: {path}\nquery: {query}");
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+    let parsed_file = read_file(reader, &query)?;
+    let lines_containing = parsed_file.0;
+    let matches = parsed_file.1;
+    println!("Found {matches} matches...");
+    for line in lines_containing {
+        println!("{}: {}", line.0, line.1);
+    }
+
+    Ok(())
+}
+
+fn read_file(reader: BufReader<File>, query: &str) -> Result<(Vec<(i32, String)>, i32), Error> {
+    let mut line_number = 0;
+    let mut matches = 0;
+    let mut lines_containing: Vec<(i32, String)> = Vec::new();
+    for line in reader.lines() {
+        let line = line?;
+        let line = line.trim();
+        if line.contains(&query) {
+            lines_containing.push((line_number, line.to_string()));
+            matches += 1;
+        }
+        line_number += 1;
+    }
+    Ok((lines_containing, matches))
 }
 
 // Add input validation
@@ -14,8 +44,8 @@ fn get_path() -> String {
     io::stdin()
         .read_line(&mut path)
         .expect("Failed to read input");
-    let path = path.trim();
-    path.to_string()
+    let path = path.trim().to_string();
+    path
 }
 
 // Add input validation
