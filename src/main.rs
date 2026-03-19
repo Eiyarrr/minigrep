e74e748ff0a59057e7a95ff492a6d9405a63cde9
Eiyarrr <eiyar@eiyar.com>
2026-03-19 19:20:23 +0000

    Fixed parsing of path (last commit broke)

--- src/main.rs
+++ src/main.rs
@@ -42,17 +42,22 @@ fn read_file(reader: BufReader<File>, query: &str) -> Result<(Vec<(i32, String)>
 }
 
 fn get_file() -> File {
+    clearscreen::clear().expect("Failed to clear console");
     loop {
-        clearscreen::clear().expect("Failed to clear console");
         println!("Enter your file path: ");
         let mut path = String::new();
         io::stdin()
             .read_line(&mut path)
             .expect("Failed to read input");
         let path = path.trim().to_string();
-        match File::open(path) {
-            Ok(file) => { file },
-            Err(_) => { continue; }
+        match File::open(&path) {
+            Ok(file) => {
+                return file;
+            }
+            Err(_) => {
+                println!("Could not find file at {path}");
+                continue;
+            }
         };
     }
 }
